45789671bb701386461e8dd79ff12c18e85dbc27
y-zumi <b1501498@planet.kanazawa-it.ac.jp>
2017-11-27 23:56:04 +0900

    Created list sample

---
+++ Chapter1/list.hs
@@ -0,0 +1,9 @@
+main = do
+    let lostNumbers = [4, 8, 15, 16, 23, 42]
+    print lostNumbers
+    let linkList = [30, 2, 5, 19] ++ [9, 10, 3, 5, 8]
+    print linkList
+    let linkString = "hello" ++ " " ++ "world"
+    let linkStringList = ['H', 'o'] ++ ['o', 'o']
+    print linkString
+    print linkStringList
