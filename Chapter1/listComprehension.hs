55d8ad30dea619f0c0746da3cbe589add10dcd7a
y-zumi <b1501498@planet.kanazawa-it.ac.jp>
2017-12-01 17:27:45 +0900

    Added length function.

--- Chapter1/listComprehension.hs
+++ Chapter1/listComprehension.hs
@@ -1,6 +1,6 @@
 -- x <- xs でリストxsをxに代入しているので引数はlistだよねー
 boomBangs xs = [ if x < 10 then "Boom!!" else "Bang!!" | x <- xs , odd x] -- oddは奇数の時にTrueを返す
-
+length' xs = sum [1 | _ <- xs]
 main = do
     -- リストから要素を取り出し，xに束縛する．｜より前の部分は出力を表す
     print([x*2 | x <- [1..10]])
@@ -14,3 +14,4 @@ main = do
     let nouns = ["hobo", "frog", "pope"]
     let adjectives = ["lazy", "grouchy", "scheming"]
     print([adjective ++ " " ++ noun | adjective <- adjectives, noun <- nouns])
+    print(length' nouns)
