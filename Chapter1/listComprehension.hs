3b81fe960e926f07f92042dcfe2c7b38435dcd63
y-zumi <b1501498@planet.kanazawa-it.ac.jp>
2017-12-01 17:24:57 +0900

    Added comprehension of string

--- Chapter1/listComprehension.hs
+++ Chapter1/listComprehension.hs
@@ -8,3 +8,9 @@ main = do
     print([x | x <- [50..100], x `mod` 7 == 3]) -- 述語を使ってリストを間引くことをフィルタするという
     print(boomBangs [1..20])
     print([x | x <- [10..20], x /= 13, x /= 15, x /= 19]) -- /= is !=
+    print([ x+y | x <- [1,2,3], y <- [10,100,1000]])
+    print([ x*y | x <- [2,5,10], y <- [8,10,11]])
+    print([ x*y | x <- [2,5,10], y <- [8,10,11], x*y > 50])
+    let nouns = ["hobo", "frog", "pope"]
+    let adjectives = ["lazy", "grouchy", "scheming"]
+    print([adjective ++ " " ++ noun | adjective <- adjectives, noun <- nouns])
