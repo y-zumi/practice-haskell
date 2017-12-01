8472258722c9e90d9c7c650640b8338d74219aa6
y-zumi <b1501498@planet.kanazawa-it.ac.jp>
2017-12-01 18:30:57 +0900

    Added tuple of zip

--- Chapter1/tuple.hs
+++ Chapter1/tuple.hs
@@ -6,3 +6,7 @@ main = do
     print (fst ("Wow",False))
     print (snd (8,11))          -- snd second
     print (snd ("Wow",False))
+    print (zip [1,2,3,4,5] [5,5,5,5,5])
+    print (zip [1..5] ["one","two","three","four","five"])
+    print (zip [5,3,2,6,2,7,2,5,4,6,6] ["im","a","turtle"])
+    print (zip [1..] ["apple","orange","cherry","mango"])
