ea9f424acaebf8133ee80d280b914cebd90c26a8
y-zumi <b1501498@planet.kanazawa-it.ac.jp>
2017-12-01 18:19:08 +0900

    Added syntax of fst and snd

--- Chapter1/tuple.hs
+++ Chapter1/tuple.hs
@@ -2,3 +2,7 @@ main = do
     print (1,3)
     print (3,'a',"hello")
     print (50,50.4,"hello",'b')
+    print (fst (8,11))          -- fst first
+    print (fst ("Wow",False))
+    print (snd (8,11))          -- snd second
+    print (snd ("Wow",False))
