b6a3f53d12cefdd5fc548cfae346c7e8b055a2b6
y-zumi <b1501498@planet.kanazawa-it.ac.jp>
2017-12-01 17:46:37 +0900

    Added removeNonUppercase

--- Chapter1/listComprehension.hs
+++ Chapter1/listComprehension.hs
@@ -1,6 +1,8 @@
 -- x <- xs でリストxsをxに代入しているので引数はlistだよねー
 boomBangs xs = [ if x < 10 then "Boom!!" else "Bang!!" | x <- xs , odd x] -- oddは奇数の時にTrueを返す
 length' xs = sum [1 | _ <- xs]
+removeNonUppercase st = [c | c <- st, c `elem` ['A'..'Z']]
+
 main = do
     -- リストから要素を取り出し，xに束縛する．｜より前の部分は出力を表す
     print([x*2 | x <- [1..10]])
@@ -15,3 +17,4 @@ main = do
     let adjectives = ["lazy", "grouchy", "scheming"]
     print([adjective ++ " " ++ noun | adjective <- adjectives, noun <- nouns])
     print(length' nouns)
+    print(removeNonUppercase "Hey Guys!!")
