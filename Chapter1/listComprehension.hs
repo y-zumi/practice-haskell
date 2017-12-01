67d5cb358a76531f037d4671999c4f4b0b1c1cf6
y-zumi <b1501498@planet.kanazawa-it.ac.jp>
2017-12-01 18:00:56 +0900

    Added even program

--- Chapter1/listComprehension.hs
+++ Chapter1/listComprehension.hs
@@ -1,7 +1,7 @@
 -- x <- xs でリストxsをxに代入しているので引数はlistだよねー
 boomBangs xs = [ if x < 10 then "Boom!!" else "Bang!!" | x <- xs , odd x] -- oddは奇数の時にTrueを返す
 length' xs = sum [1 | _ <- xs]
-removeNonUppercase st = [c | c <- st, c `elem` ['A'..'Z']]
+removeNonUppercase st = [c | c <- st, c `elem` ['A'..'Z']] -- elem : 指定した要素がリストに含まれているかを調べる
 
 main = do
     -- リストから要素を取り出し，xに束縛する．｜より前の部分は出力を表す
@@ -18,3 +18,7 @@ main = do
     print([adjective ++ " " ++ noun | adjective <- adjectives, noun <- nouns])
     print(length' nouns)
     print(removeNonUppercase "Hey Guys!!")
+    let xxs = [[1,3,5,2,3,1,2,4,5],
+               [1,2,3,4,5,6,7,8,9],
+               [1,2,4,2,1,6,3,1,3,2,3,6]]
+    print([[x | x <- xs, even x] | xs <- xxs]) -- evenは偶数の時にTrueを返す
