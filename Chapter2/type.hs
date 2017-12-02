9964fac8c1881a5365b1c0a110152177f0c07ca4
y-zumi <b1501498@planet.kanazawa-it.ac.jp>
2017-12-02 20:01:54 +0900

    Added three argument function

--- Chapter2/type.hs
+++ Chapter2/type.hs
@@ -1,6 +1,9 @@
 removeNonUppercase :: [Char] -> [Char]
+addThree :: Int -> Int -> Int -> Int
 
 main = do
     print(removeNonUppercase ['A'..'J'])
-    
+    print(addThree 1 2 3)
+
 removeNonUppercase st = [c | c <- st, c `elem` ['A'..'Z']]
+addThree x y z = x + y + z
