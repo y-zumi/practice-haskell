4c8f7c6e2c3c0177ab07a3fae78cc6d948057f18
y-zumi <b1501498@planet.kanazawa-it.ac.jp>
2017-12-14 00:08:40 +0900

    Added compare hundred

--- Chapter5/multThree.hs
+++ Chapter5/multThree.hs
@@ -1,6 +1,10 @@
 --multThree :: Int -> Int -> Int
 multThree x y z = x * y * z
+compareWithHundred x = compare 100 x
+compareWithHundred' = compare 100
 
 main = do
     let multTwoWithNine = multThree 3
     print(multTwoWithNine 2 3)
+    print(compareWithHundred 99)
+    print(compareWithHundred' 100)
